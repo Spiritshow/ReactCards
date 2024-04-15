bf11069c53d4575199cea2f86bca2b01e8270252
Spirit <vkozyaychev@mail.ru>
2024-04-15 12:10:41 +0300

    edit app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+const props = {
+  title: "1",
+  description: "2"
+}
+
+const AbstractImg = () => {
+  return(
+    <img src= {logo} className='App-logo' alt="logo"/>
+  );
+}
+
+const Title = (text) =>{
+  return(
+    <h4 className='text'>{text}</h4>
+  );
+}
+
+const Description = (text) => {
+  return(
+    <p className='text'>{text}</p>
+  );
+}
+
+const Textcard = (title,description) =>{
+  return(
+    <div className='textcard'>
+      {title}
+      {description}
+    </div>
+  );
+}
+
+const Card = (image='./src/components/images/penpen.jpg',textcard) => {
+  return(
+    <div className='cardCastom'>
+      <img src={image} className='image' alt='logo'/>
+      {textcard}
+    </div>
+  );
+}
+
 function App() {
   return (
+    <> 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AbstractImg/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
+        </p>
       </header>
+      <div className="App-body">
+      <Card textcard={<Textcard title={<Title text='1'/>} description={<Description text="2"/>}/>}/>
+      </div>
     </div>
+    </>
   );
 }
 
